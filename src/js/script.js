135b13e069643b19c49f14458f60535f979aec36
Vasiliy <vasyasjob@gmail.com>
2019-07-25 16:52:38 +0300

    initial project setup

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+require('es6-promise').polyfill();
+require('nodelist-foreach-polyfill'); 
+require('formdata-polyfill');
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+
+});
